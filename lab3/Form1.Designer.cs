d4315b120372198a64238e2debdd2ee53850963d
charish-seyo <c.dacillo.544158@umindanao.edu.ph>
2026-04-26 11:49:26 +0800

    Updated Features

--- lab3/Form1.Designer.cs
+++ lab3/Form1.Designer.cs
@@ -20,20 +20,52 @@
 			base.Dispose(disposing);
 		}
 
-		#region Windows Form Designer generated code
+        #region Windows Form Designer generated code
 
-		/// <summary>
-		///  Required method for Designer support - do not modify
-		///  the contents of this method with the code editor.
-		/// </summary>
-		private void InitializeComponent()
-		{
-			this.components = new System.ComponentModel.Container();
-			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(800, 450);
-			this.Text = "Form1";
-		}
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            lstBookInfo = new ListBox();
+            btnShowInfo = new Button();
+            SuspendLayout();
+            // 
+            // lstBookInfo
+            // 
+            lstBookInfo.FormattingEnabled = true;
+            lstBookInfo.ItemHeight = 15;
+            lstBookInfo.Location = new Point(23, 54);
+            lstBookInfo.Name = "lstBookInfo";
+            lstBookInfo.Size = new Size(296, 274);
+            lstBookInfo.TabIndex = 0;
+            // 
+            // btnShowInfo
+            // 
+            btnShowInfo.Location = new Point(23, 25);
+            btnShowInfo.Name = "btnShowInfo";
+            btnShowInfo.Size = new Size(75, 23);
+            btnShowInfo.TabIndex = 1;
+            btnShowInfo.Text = "Show Info";
+            btnShowInfo.UseVisualStyleBackColor = true;
+            btnShowInfo.Click += btnShowInfo_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(526, 412);
+            Controls.Add(btnShowInfo);
+            Controls.Add(lstBookInfo);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+        }
 
-		#endregion
-	}
+        #endregion
+
+        private ListBox lstBookInfo;
+        private Button btnShowInfo;
+    }
 }
--- lab3/Form1.cs
+++ lab3/Form1.cs
@@ -1,10 +1,115 @@
 namespace lab3
 {
-	public partial class Form1 : Form
-	{
-		public Form1()
-		{
-			InitializeComponent();
-		}
-	}
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public class Book
+        {
+            public string Title { get; set; }
+            public string Author { get; set; }
+
+            public virtual string GetInfo()
+            {
+                return $"{Title} by {Author}";
+            }
+        }
+
+        public class Magazine : Book
+        {
+            public int IssueNumber { get; set; }
+
+            public override string GetInfo()
+            {
+                return $"{Title} by {Author} - Issue {IssueNumber}";
+            }
+        }
+
+        public class Ebook : Book
+        {
+            public double FileSizeMB { get; set; }
+
+            public override string GetInfo()
+            {
+                return $"{Title} by {Author} - Ebook Size: {FileSizeMB} MB";
+            }
+        }
+
+        public class Textbook : Book
+        {
+            public string Subject { get; set; }
+
+            public override string GetInfo()
+            {
+                return $"{Title} by {Author} - Subject: {Subject}";
+            }
+        }
+
+        public class AudioBook : Book
+        {
+            public double Duration { get; set; }
+            public string Narrator { get; set; }
+
+            public override string GetInfo()
+            {
+                return $"{Title} by {Author} - Duration: {Duration} hours, Narrator: {Narrator}";
+            }
+        }
+
+        public void DisplayBooks(Book[] books)
+        {
+            lstBookInfo.Items.Clear();
+
+            foreach (Book book in books)
+            {
+                lstBookInfo.Items.Add(book.GetInfo());
+            }
+        }
+
+        private void btnShowInfo_Click(object sender, EventArgs e)
+        {
+            Book[] books = new Book[]
+            {
+                new Book
+                {
+                    Title = "Introduction to Programming",
+                    Author = "Robert Smith"
+                },
+
+                new Magazine
+                {
+                    Title = "National Geographic",
+                    Author = "John Doe",
+                    IssueNumber = 45
+                },
+
+                new Ebook
+                {
+                    Title = "C# Basics",
+                    Author = "Maria Santos",
+                    FileSizeMB = 3.5
+                },
+
+                new Textbook
+                {
+                    Title = "Object-Oriented Programming",
+                    Author = "Anna Reyes",
+                    Subject = "Computer Programming"
+                },
+
+                new AudioBook
+                {
+                    Title = "Learning C#",
+                    Author = "Mark Johnson",
+                    Duration = 5.5,
+                    Narrator = "David Lee"
+                }
+            };
+
+            DisplayBooks(books);
+        }
+    }
 }
